bf6604af2650bea3f464e6c75a3a087f3be91096
miorajulia01 <miorahanitra01.com>
2026-02-19 11:05:16 +0300

    ajout de tax_config

--- src/main/resources/sql/schéma.sql
+++ src/main/resources/sql/schéma.sql
@@ -15,7 +15,11 @@ CREATE TABLE invoice_line (
     unit_price NUMERIC(10,2) NOT NULL
 );
 
-
+CREATE TABLE tax_config (
+    id SERIAL PRIMARY KEY,
+    label VARCHAR NOT NULL,
+    rate NUMERIC(5,2) NOT NULL
+);
 
 
 
--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -9,3 +9,6 @@ INSERT INTO invoice_line (invoice_id, label, quantity, unit_price) VALUES
     (2, 'Produit A', 5, 100),
     (2, 'Service C', 1, 200),
     (3, 'Produit B', 3, 50);
+
+INSERT INTO tax_config (label, rate) VALUES
+('TVA STANDARD', 20.00);
